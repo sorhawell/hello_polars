14e31b8ebfa85b9ebd596fb0d85f4401e9061cdc
sorhawell <sorhawell@gmail.com>
2023-02-03 09:27:13 +0100

    simple hello polars

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,9 +1,14 @@
 use extendr_api::prelude::*;
+use polars::prelude::*;
 
 /// Return string `"Hello world!"` to R.
 /// @export
 #[extendr]
 fn hello_world() -> &'static str {
+    let s = Series::new("hello polars", &[1, 2, 3]);
+
+    rprintln!("this is a polars series {:?}", s);
+
     "Hello world!"
 }
 
